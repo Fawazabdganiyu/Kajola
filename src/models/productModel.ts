ac27259abf4c59a55cc461f877c0ac4774fec0f2
Fawaz Abdganiyu <117312471+Fawazabdganiyu@users.noreply.github.com>
2024-05-20 22:06:13 +0100

    Define Mongoose models
    * Update dependencies
    
    * Define mongoose models
    
    The models are:
    - userModel
    - productModel
    - reviewModel
    
    Then add test the productModel and its included method
    
    * Add more productModel test

---
+++ src/models/productModel.ts
@@ -0,0 +1,62 @@
+import { Schema, model } from 'mongoose';
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  negotiable: {
+    type: Boolean,
+    default: true,
+  },
+  ratings: [{
+    type: Number,
+    enum: [0, 1, 2, 3, 4, 5],
+  }],
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  reviewCount: {
+    type: Number,
+    default: 0,
+  },
+  desc: String,
+  images: [String],
+}, {
+  timestamps: true
+});
+
+productSchema.methods.addReview = function (rating: number) {
+  // Add the new rating to the ratings array
+  this.ratings.push(rating);
+
+  // Increment the reviewCount
+  this.reviewCount += 1;
+
+  // Calculate the new averageRating
+  const sum = this.ratings.reduce((acc: number, curr: number) => acc + curr, 0);
+  this.averageRating = sum / this.ratings.length;
+
+  // Save the updated product
+  return this.save();
+}
+
+export default model('Product', productSchema);
---
+++ src/models/userModel.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  userType: {
+    type: String,
+    enum: ['Buyer', 'Seller'],
+    default: 'Buyer',
+  },
+  desc: String,
+  img: String,
+}, {
+  timestamps: true
+});
+
+export default model('User', userSchema);
