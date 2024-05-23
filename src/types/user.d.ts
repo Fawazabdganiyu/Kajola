3da0845048dab6c58b4dd09d4dad1273b3e51531
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-05-23 06:42:58 +0100

    Update UserSchema type inference

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,7 +1,7 @@
 import { Document } from 'mongoose';
 
 export interface IUser extends Document {
-  _id: string;
+  _id: Schema.Types.ObjectId;
   firstName: string;
   lastName: string;
   email: string;
