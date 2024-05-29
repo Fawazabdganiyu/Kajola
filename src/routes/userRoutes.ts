57b379d0e4787606d0b70e3b118ee968f66605d3
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-05-29 01:45:54 +0100

    Resolve endpoints conflict
    I used regex to differentiate /api/users/me from /api/users/:id
    and also by placing .../me above the .../:id

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,8 +4,8 @@ import authMiddleware from '../middlewares/authMiddleware';
 
 const userRouter = Router();
 
+userRouter.get(/me$/, authMiddleware, UsersController.getMe);
 userRouter.get('/:id', UsersController.getUser);
-userRouter.get('/me', authMiddleware, UsersController.getMe);
 userRouter.put('/:id', authMiddleware, UsersController.updateUser);
 userRouter.delete('/:id', authMiddleware, UsersController.deleteUser);
 
